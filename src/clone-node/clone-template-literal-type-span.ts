a164d27c65c758f940655087f3195aa79b621533
wessberg <frederikwessberg@gmail.com>
2020-10-21 01:01:58 +0200

    fix: add support for 4.1.0-beta and 4.1.0 nightly

--- src/clone-node/clone-template-literal-type-span.ts
+++ src/clone-node/clone-template-literal-type-span.ts
@@ -2,9 +2,19 @@ import {TS} from "./type/ts";
 import {CloneNodeVisitorOptions} from "./clone-node-options";
 
 export function cloneTemplateLiteralTypeSpan(node: TS.TemplateLiteralTypeSpan, options: CloneNodeVisitorOptions<TS.TemplateLiteralTypeSpan>): TS.TemplateLiteralTypeSpan {
-	return options.factory.createTemplateLiteralTypeSpan(
-		options.hook("casing", node.casing, node.casing),
-		options.hook("type", options.nextNode(node.type), node.type),
-		options.hook("literal", options.nextNode(node.literal), node.literal)
-	);
+	if ("casing" in node && options.factory.createTemplateLiteralTypeSpan.length === 3) {
+		const legacyNode = (node as unknown) as import("typescript-4-1-0-beta").TemplateLiteralTypeSpan;
+		const legacyCreateTemplateLiteralTypeSpan = (options.factory as unknown) as import("typescript-4-1-0-beta").NodeFactory["createTemplateLiteralTypeSpan"];
+
+		return (legacyCreateTemplateLiteralTypeSpan(
+			options.hook("casing" as never, legacyNode.casing as never, legacyNode.casing as never) as never,
+			options.hook("type", options.nextNode(legacyNode.type as never), legacyNode.type as never) as never,
+			options.hook("literal", options.nextNode(legacyNode.literal as never), legacyNode.literal as never) as never
+		) as unknown) as TS.TemplateLiteralTypeSpan;
+	} else {
+		return options.factory.createTemplateLiteralTypeSpan(
+			options.hook("type", options.nextNode(node.type), node.type),
+			options.hook("literal", options.nextNode(node.literal), node.literal)
+		);
+	}
 }
